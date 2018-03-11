5e0488bc3528adf78f12c02c5b7ae0d05674bc9a
UeharaSorane <34863466+UeharaSorane@users.noreply.github.com>
2018-03-11 15:36:19 +0800

    Update analytics.js

--- modules/analytics.js
+++ modules/analytics.js
@@ -86,7 +86,8 @@ function parseInput(rplyToken, inputStr) {
 	if (trigger.match(/立flag|死亡flag/) != null) return exports.funny.BStyleFlagSCRIPTS() ;	
 	
 	//鴨霸獸指令開始於此
-	if (trigger.match(/沙希醬|沙希/) != null) return exports.funny.randomReply() ;	
+	if (trigger.match(/沙希醬|沙希/) != null) return exports.funny.randomReply() ;
+	if (trigger.match(/C8763|星爆氣流斬/) != null) return exports.funny.C8763() ;
 	if (trigger.match(/運氣|運勢/) != null) return exports.funny.randomLuck(mainMsg) ; //占卜運氣		
 	
 
