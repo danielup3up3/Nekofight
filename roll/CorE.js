b181da1b8eeeabde309c2ea14b6376f1dcf5eeb9
danielup3up3 <37258273+danielup3up3@users.noreply.github.com>
2018-03-16 23:39:57 +0800

    Update CorE.js

--- roll/CorE.js
+++ roll/CorE.js
@@ -2,32 +2,31 @@ var rollbase = require('./rollbase.js');
 var rply ={type : 'text'}; //type是必需的,但可以更改
 
 //////////////// 核心建造
-	function main(Gmoney,StarStone,Decore) 
+	function main(Gmoney,StarStone,Decore) {
   
   ///基本變數(不要動)
 		///基本變數
-
-		let BonusGmoney = [];
-                let BonusStarStone = [];
-                let BonusDecore = [];
+		let Gmoneylist = [];
+                let Stonelist = [];
+                let Decorelist = [];
     
     
     //判定
     
     if(Gmoney < 50 && Gmoney >= 10){
     
-    BonusGmoney.length = 6;
-    BonusGmoney = ['☆A核心','☆B核心','☆☆H核心','☆☆☆☆S核心','☆☆K核心','☆☆☆M核心'];
+    Gmoneylist.length = 6;
+    Gmoneylist = ['☆A核心','☆B核心','☆☆H核心','☆☆☆☆S核心','☆☆K核心','☆☆☆M核心'];
     
     if(StarStone < 5 && StarStone >= 0){
     
-    BonusStarStone.length = 7;
-    BonusStarStone = ['☆A核心','☆☆C核心','☆☆☆☆J核心','☆B核心','☆F核心','☆☆E核心','☆☆☆L核心'];
+    Stonelist.length = 7;
+    Stonelist = ['☆A核心','☆☆C核心','☆☆☆☆J核心','☆B核心','☆F核心','☆☆E核心','☆☆☆L核心'];
     
     if(Decore <= 100 && Decore >= 10){
     
-    BonusDecore.length = 6;
-    BonusDecore = ['☆G核心','☆I核心','☆☆H核心','☆☆☆☆☆N核心','☆☆☆M核心','☆☆☆L核心'];
+    Decorelist.length = 6;
+    Decorelist = ['☆G核心','☆I核心','☆☆H核心','☆☆☆☆☆N核心','☆☆☆M核心','☆☆☆L核心'];
     
     }
     }
@@ -35,18 +34,18 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
     
     }else if(Gmoney < 150 && Gmoney >= 50){
     
-    BonusGmoney.length = 4;
-    BonusGmoney = ['☆☆C核心','☆☆☆D核心','☆G核心','☆☆☆☆☆N核心'];
+    Gmoneylist.length = 4;
+    Gmoneylist = ['☆☆C核心','☆☆☆D核心','☆G核心','☆☆☆☆☆N核心'];
     
     if(StarStone < 10 && StarStone >= 5){
     
-    BonusStarStone.length = 4;
-    BonusStarStone = ['☆☆☆D核心','☆☆H核心','☆☆☆M核心','☆☆K核心'];
+    Stonelist.length = 4;
+    Stonelist = ['☆☆☆D核心','☆☆H核心','☆☆☆M核心','☆☆K核心'];
     
     if(Decore <= 250 && Decore > 100){
     
-    BonusDecore.length = 5;
-    BonusDecore = ['☆A核心','☆☆C核心','☆☆☆D核心','☆☆☆E核心','☆F核心'];
+    Decorelist.length = 5;
+    Decorelist = ['☆A核心','☆☆C核心','☆☆☆D核心','☆☆☆E核心','☆F核心'];
     
     }
     }
@@ -54,18 +53,18 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
     
      }else if(Gmoney < 300 && Gmoney >= 150){
     
-    BonusGmoney.length = 5;
-    BonusGmoney = ['☆☆E核心','☆F核心','☆I核心','☆☆☆☆J核心','☆☆☆L核心'];
+    Gmoneylist.length = 5;
+    Gmoneylist = ['☆☆E核心','☆F核心','☆I核心','☆☆☆☆J核心','☆☆☆L核心'];
     
      if(StarStone >= 10){
     
-    BonusStarStone.length = 3;
-    BonusStarStone = ['☆G核心','☆I核心','☆☆☆☆☆N核心'];
+    Stonelist.length = 3;
+    Stonelist = ['☆G核心','☆I核心','☆☆☆☆☆N核心'];
     
      if(Decore > 250){
     
-    BonusDecore.length = 3;
-    BonusDecore = ['☆B核心','☆☆K核心','☆☆☆☆J核心'];
+    Decorelist.length = 3;
+    Decorelist = ['☆B核心','☆☆K核心','☆☆☆☆J核心'];
     
     }
     }
@@ -80,9 +79,9 @@ var rply ={type : 'text'}; //type是必需的,但可以更改
     
     let CorEresult = [];
 
-CorEresult.length = BonusGmoney.length + BonusStarStone.length  + BonusDecore.length;
+CorEresult.length = Gmoneylist.length + Stonelist.length  + Decorelist.length;
 
-CorEresult = BonusGmoney.concat(BonusStarStone,BonusDecore);
+CorEresult = Gmoneylist.concat(Stonelist,Decorelist);
 
 
     
@@ -93,7 +92,7 @@ return rply;
 
     
 
-
+	}
 ////////////////
 
 module.exports = {
